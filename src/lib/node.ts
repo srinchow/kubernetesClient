2b1cb868e934cb327435c81671571e819ab1fd66
Srinjoy Choudhury <srinchow@gmail.com>
2023-02-12 21:20:25 +0530

    removing node changes

--- src/lib/node.ts
+++
@@ -1,9 +0,0 @@
-import { appApi, coreApi } from "../client/client";
-
-
-export const getNodes = async (nodeName: string, namespaced = "default") => {
-    const node = await coreApi.listNode(undefined, false, undefined, `metadata.name=${nodeName}`);
-    return node.body.items;
-}
-
-
