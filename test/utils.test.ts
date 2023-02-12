bc8a1d72e62068a6e509f1ef020f929c52002576
Srinjoy Choudhury <srinchow@gmail.com>
2023-02-12 21:26:40 +0530

    refactor test

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -3,19 +3,19 @@ import { describe, expect, test, jest } from '@jest/globals';
 
 describe("Unit Conversion", () => {
     test("Cpu Unit Conversion", () => {
-        const cpu: k8CPU = "4";
-        const result = convertUnit(cpu);
-        expect(Number(result)).toEqual(4);
-
-        const cpu1: k8CPU = "400m";
-        const result1 = convertUnit(cpu1);
-        expect(Number(result1)).toEqual(0.4);
-
+        const testcases = {
+            "4": 4,
+            "400m": 0.4
+        }
+        for (const [testcase, expectedValue] of Object.entries(testcases)) {
+            const result = Number(convertUnit(testcase))
+            expect(result).toEqual(expectedValue);
+        }
     });
 
 
     test("Memory Unit Conversion", () => {
-        const cases = {
+        const testcases = {
             "123Mi": 128974848,
             "123M": 123000000,
             "123k": 123000,
@@ -24,7 +24,7 @@ describe("Unit Conversion", () => {
             "123000m": 123,
         }
 
-        for (const [testcase, expectedValue] of Object.entries(cases)) {
+        for (const [testcase, expectedValue] of Object.entries(testcases)) {
             const result = Number(convertUnit(testcase))
             expect(result).toEqual(expectedValue);
         }
